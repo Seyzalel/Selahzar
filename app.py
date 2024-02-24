ae8651d041410b4642cc6f94a5e53201be385c75
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2024-02-24 14:35:30 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import telebot
+from telebot import types
 
-TOKEN = '6788247666:AAE_9h3yeE6uRHz__MnJs5UXBsbtR4pj1JA'  # Substitua YOUR_TOKEN_HERE pelo token fornecido pelo BotFather
-bot = telebot.TeleBot(TOKEN)
+# Direitos reservados 2024, Empresa Selahzar S.A.
+__author__ = "Selahzar S.A."
+__copyright__ = "2024 Selahzar S.A."
+__license__ = "Private"
+__version__ = "1.0"
 
+# Token do bot
+selahzar_telegram_bot_token = '6788247666:AAE_9h3yeE6uRHz__MnJs5UXBsbtR4pj1JA'
+bot = telebot.TeleBot(selahzar_telegram_bot_token)
+
+# Comando /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    username = message.from_user.username
-    bot.reply_to(message, f"Hello, @{username}")
+    # Criando o botão
+    markup = types.InlineKeyboardMarkup()
+    instagram_button = types.InlineKeyboardButton("Instagram", url="https://instagram.com")
+    markup.add(instagram_button)
 
-bot.polling()
+    # Enviando a mensagem com o botão
+    bot.send_message(message.chat.id, "Bem-vindo ao bot da Selahzar S.A.! Clique abaixo para nos visitar no Instagram.", reply_markup=markup)
+
+# Mantém o bot rodando
+bot.infinity_polling()
