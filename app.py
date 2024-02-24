7ffd5bbbac7d093d83a97b6392dd9f7b1f5b1984
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2024-02-24 20:15:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import telebot
-from telebot import types, apihelper
+from telebot import types
+import requests
+import threading
 
 # Direitos reservados 2024, Empresa Selahzar S.A.
 __author__ = "Selahzar S.A."
@@ -11,20 +13,31 @@ __version__ = "1.0"
 selahzar_telegram_bot_token = '6788247666:AAE_9h3yeE6uRHz__MnJs5UXBsbtR4pj1JA'
 bot = telebot.TeleBot(selahzar_telegram_bot_token)
 
+# Função para visitar o site
+def visit_site():
+    url = "https://selahzar-1.onrender.com/"
+    headers = {"User-Agent": "KeepAliveAgent"}
+    try:
+        response = requests.get(url, headers=headers)
+        print(f"Site visited with status code: {response.status_code}")
+    except Exception as e:
+        print(f"Failed to visit site: {e}")
+    # Agendar a próxima visita
+    threading.Timer(7, visit_site).start()
+
 # Comando /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    # Criando o botão
     markup = types.InlineKeyboardMarkup()
     instagram_button = types.InlineKeyboardButton("Instagram", url="https://instagram.com")
     markup.add(instagram_button)
+    bot.send_message(message.chat.id, "Bem-vindo ao bot da Selahzar S.A.! Clique abaixo para nos visitar no Instagram.", reply_markup=markup)
 
-    # Enviando a mensagem com o botão
-    bot.send_message(message.chat.id, "Bem-vindo ao bot da Selahzar S.A.! Clique abaixo para nos visitar no Instagram. ", reply_markup=markup)
+# Iniciar a visita ao site
+visit_site()
 
 # Tentativa de manter o bot rodando e lidar com exceções
 try:
     bot.infinity_polling()
-except apihelper.ApiTelegramException as e:
+except telebot.apihelper.ApiTelegramException as e:
     print(f"Erro detectado: {e}")
-    # Lógica adicional para lidar com o erro ou reiniciar o bot pode ser adicionada aqui.
