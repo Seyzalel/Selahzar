f358d977b4b226456c37dc99bf69ae76baf8fbee
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2024-02-24 21:44:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-print("Hello World!")
+from telegram.ext import Updater, CommandHandler
+
+# Função para responder ao comando /start
+def start(update, context):
+    # Enviar a mensagem "Hello World!"
+    update.message.reply_text('Hello World!')
+
+def main():
+    # Inicializar o updater com o token do seu bot
+    updater = Updater("INSIRA_SEU_TOKEN_AQUI", use_context=True)
+
+    # Obter o despachante para registrar os manipuladores
+    dp = updater.dispatcher
+
+    # Adicionar um manipulador para o comando /start
+    dp.add_handler(CommandHandler("start", start))
+
+    # Iniciar o bot
+    updater.start_polling()
+
+    # Manter o bot em execução até que Ctrl + C seja pressionado
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
