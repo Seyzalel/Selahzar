dc083b8d413a9d0521df5e5c620e2e54af179c58
Seyzalel Menabel <127889263+Seyzalel@users.noreply.github.com>
2024-02-25 00:43:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,37 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes, JobQueue
+from telegram import Update, Bot
+from telegram.ext import Application, CommandHandler, ContextTypes, CallbackContext
+import logging
+
+# Insira o ID do seu grupo aqui
+GROUP_CHAT_ID = -4195474080
+
+# Token do bot fornecido pelo BotFather
+TOKEN = "6788247666:AAE_9h3yeE6uRHz__MnJs5UXBsbtR4pj1JA"
+
+# Configuração básica do logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to Selahzar Bot!")
+    await update.message.reply_text("Hello World, Selahzar!")
 
-async def keep_alive(context: ContextTypes.DEFAULT_TYPE) -> None:
+async def keep_alive(context: CallbackContext) -> None:
     """Função para enviar uma mensagem periódica e manter o bot ativo."""
-    await context.bot.send_message(chat_id='-4195474080', text="Keep-alive message")
+    bot = context.bot
+    await bot.send_message(chat_id=GROUP_CHAT_ID, text="Keep-alive message")
 
 def main() -> None:
-    application = Application.builder().token('6788247666:AAE_9h3yeE6uRHz__MnJs5UXBsbtR4pj1JA').build()
+    # Cria uma instância do Application usando o token do bot
+    application = Application.builder().token(TOKEN).build()
 
+    # Adiciona o manipulador do comando /start
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
 
     # Adiciona a tarefa de keep-alive para ser executada a cada 5 minutos
-    jq = application.job_queue
-    jq.run_repeating(keep_alive, interval=60, first=0)
+    application.job_queue.run_repeating(keep_alive, interval=30, first=0)
 
+    # Inicia o bot
     application.run_polling()
 
 if __name__ == '__main__':
